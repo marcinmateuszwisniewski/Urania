6914d4d05df2a63f351ca24440f0cdf1ad738d82
Marcin W <marcinmateuszwisniewski@gmail.com>
2020-04-30 11:35:23 +0200

    stream in SingleTimeFactory instead of forEach

--- src/com/company/SingleTime/SingleTimeFactory.java
+++ src/com/company/SingleTime/SingleTimeFactory.java
@@ -1,23 +1,19 @@
 package com.company.SingleTime;
 
 import java.time.ZoneId;
+import java.util.Optional;
 import java.util.Set;
 
 public class SingleTimeFactory {
 
     public static SingleTimeInterface produce(String city) {
         System.out.println("start" + city);
-        String zoneId = null;
         Set<String> timezoneNames = ZoneId.getAvailableZoneIds();
-        for (String timezonesName : timezoneNames) {
-            if (timezonesName.contains(city)) {
-                System.out.println("found" + city);
-                zoneId = timezonesName;
-                break;
-            }
-        }
-        if (zoneId != null) {
-            SingleTime st = new SingleTime(ZoneId.of(zoneId), city);
+
+        Optional<String> result = timezoneNames.stream().filter(s -> s.contains(city)).findFirst();
+
+        if (result.isPresent()) {
+            SingleTime st = new SingleTime(ZoneId.of(result.get()), city);
             System.out.println("end" + city);
             return st;
         } else return null;
